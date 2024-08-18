c2efc44aeacd564b1086b24a17a94f0998cbe499
Ilya Grig <ilia.grigoriev2018@yandex.ru>
2024-08-18 12:19:19 +0400

    new1111

--- users/urls.py
+++ users/urls.py
@@ -10,7 +10,7 @@ app_name = UsersConfig.name
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="users/login.html"),
          name="login"),
-    path("logout/", LogoutView.as_view(template_name="users/logout.html")),
+    path("logout/", LogoutView.as_view(template_name="users/logout.html"),name="logout"),
     path("register/", UserCreate.as_view(), name="register"),
     path("email-confirms/<str:token>/", email_verification, name="email-confirm")
     ]
